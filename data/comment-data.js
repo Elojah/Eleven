bbf2b4912ec50c7f2e5f771c07820ec78fc1befe
Emile ROBERT <erobert@e2r12p6.42.fr>
2016-04-09 11:42:11 +0200

    fixes

--- data/comment-data.js
+++ data/comment-data.js
@@ -1,4 +1,4 @@
-var commentsArray = [
+var commentsArray0 = [
 {
    "id": 1,
    "parent": null,
@@ -19,8 +19,49 @@ var commentsArray = [
    "modified": "2015-01-02",
    "content": "Sed posuere interdum sem. Quisque ligula eros ullamcorper quis, lacinia quis facilisis sed sapien. Mauris varius diam vitae arcu.",
    "fullname": "Administrator",
+   "profile_picture_url": "https://app.viima.com/static/media/user_profiles/user-icon.png",
+   "created_by_admin": false,
+   "created_by_current_user": false,
+   "upvote_count": 2,
+   "user_has_upvoted": true
+}
+]
+var commentsArray1 = [
+{
+   "id": 1,
+   "parent": null,
+   "created": "2015-01-01",
+   "modified": "2015-01-01",
+   "content": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed posuere interdum sem. Quisque ligula eros ullamcorper quis, lacinia quis facilisis sed sapien. Mauris varius diam vitae arcu.",
+   "fullname": "Simon Powell",
    "profile_picture_url": "https://app.viima.com/static/media/user_profiles/admin-user-icon.png",
-   "created_by_admin": true,
+   "created_by_admin": false,
+   "created_by_current_user": false,
+   "upvote_count": 3,
+   "user_has_upvoted": false
+},
+{
+   "id": 2,
+   "parent": null,
+   "created": "2015-01-02",
+   "modified": "2015-01-02",
+   "content": "Sed posuere interdum sem. Quisque ligula eros ullamcorper quis, lacinia quis facilisis sed sapien. Mauris varius diam vitae arcu.",
+   "fullname": "Administrator",
+   "profile_picture_url": "https://app.viima.com/static/media/user_profiles/admin-user-icon.png",
+   "created_by_admin": false,
+   "created_by_current_user": false,
+   "upvote_count": 2,
+   "user_has_upvoted": false
+},
+{
+   "id": 3,
+   "parent": null,
+   "created": "2015-01-02",
+   "modified": "2015-01-02",
+   "content": "Sed posuere interdum sem. Quisque ligula eros ullamcorper quis, lacinia quis facilisis sed sapien. Mauris varius diam vitae arcu.",
+   "fullname": "Administrator",
+   "profile_picture_url": "https://app.viima.com/static/media/user_profiles/admin-user-icon.png",
+   "created_by_admin": false,
    "created_by_current_user": false,
    "upvote_count": 2,
    "user_has_upvoted": false
